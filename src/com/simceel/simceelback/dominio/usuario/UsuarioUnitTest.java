fcbeb46f3b750cc6c4006d202f35e5f96fbfcc92
jose <eng.jlpjunior@gmail.com>
2018-12-17 13:23:55 -0200

    Class UsuarioUnitTest.java
    alterção no codigo

--- src/com/simceel/simceelback/dominio/usuario/UsuarioUnitTest.java
+++ src/com/simceel/simceelback/dominio/usuario/UsuarioUnitTest.java
@@ -1,13 +1,86 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+package com.simceel.simceelback.dominio.usuario;
 
-/**
- *
- * @author junio
- */
-public class NewClass {
-    
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.simceel.simceelback.SimceelBackApplication;
+import io.restassured.http.ContentType;
+import io.restassured.internal.mapping.GsonMapper;
+import io.restassured.module.mockmvc.RestAssuredMockMvc;
+import io.restassured.module.mockmvc.response.MockMvcResponse;
+import io.restassured.response.Response;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.BDDMockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpStatus;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import java.util.HashMap;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.junit.Assert.assertEquals;
+import static io.restassured.module.mockmvc.RestAssuredMockMvc.*;
+import static org.hamcrest.Matchers.hasToString;
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.when;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = SimceelBackApplication.class)
+@WebAppConfiguration
+@ContextConfiguration
+public class UsuarioUnitTest {
+
+    @Autowired
+    WebApplicationContext webApplicationContext;
+
+    @MockBean
+    UsuarioRepository usuarioRepository;
+
+    private MockMvc mockMvc;
+
+    @Before
+    public void setup() {
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(this.webApplicationContext).build();
+        RestAssuredMockMvc.mockMvc(this.mockMvc);
+    }
+
+    @After
+    public void finish() {
+    }
+
+    @Test
+    public void cadastrarComDadosValidos(){
+
+        Usuario usuario = new Usuario();
+        usuario.setNome("nome");
+        usuario.setCPF("cpf");
+
+        BDDMockito.given(this.usuarioRepository.save(any(Usuario.class))).willReturn(usuario);
+
+        given()
+                .body(usuario)
+                .contentType(ContentType.JSON)
+                .when()
+                .post("/usuario")
+                .then()
+                .statusCode(HttpStatus.CREATED.value())
+                .body("nome", equalTo("LeoGodoi"))
+                .body("cpf", equalTo("711498523"));
+
+    }
+
+
+
 }

4a7cf3508c940fe77bd662d8efb77f6c4a5665fc
jose <eng.jlpjunior@gmail.com>
2018-12-17 12:46:34 -0200

    Class SimceelBackApplication.java criada

---
+++ src/com/simceel/simceelback/SimceelBackApplication.java
@@ -0,0 +1,12 @@
+package com.simceel.simceelback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimceelBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimceelBackApplication.class, args);
+	}
+}

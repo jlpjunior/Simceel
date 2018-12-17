294a7f2eee0cffcf56a6f495f02aac0d5f43e954
jose <eng.jlpjunior@gmail.com>
2018-12-16 23:25:58 -0200

    nova variavel nome

--- src/simceel/Simceel.java
+++ src/simceel/Simceel.java
@@ -15,6 +15,8 @@ public class Simceel {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        
+        private nome;
         // TODO code application logic here
     }
     
